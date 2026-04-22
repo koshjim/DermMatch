4f66ced7884163b0606f19f8603c85dc4f2770b2
catherine-fang-git <157651589+catherine-fang-git@users.noreply.github.com>
2026-04-22 19:41:03 -0400

    Revert "Revert "trying to fix builld_search_index"" aaa This reverts commit c4e43fec5d213f426cddf1132fc3d6aeeedf0036.

--- src/routes.py
+++ src/routes.py
@@ -383,14 +383,14 @@ def build_search_index():
 
     products = Product.query.all()
 
-    corpus = [" ".join(tokenize_and_stem(_product_svd_text(p))) for p in products]
+    # corpus = [" ".join(tokenize_and_stem(_product_svd_text(p))) for p in products]
     
-    # corpus = []
-    # for p in products:
-    #     raw_text = _product_svd_text(p) or ""
-    #     tokens = tokenize_and_stem(raw_text)
-    #     if tokens:
-    #         corpus.append(" ".join(tokens))
+    corpus = []
+    for p in products:
+        raw_text = _product_svd_text(p) or ""
+        tokens = tokenize_and_stem(raw_text)
+        if tokens:
+            corpus.append(" ".join(tokens))
 
     if not corpus:
         raise ValueError("Corpus is empty — cannot build search index")
